1804f573c3fb7b507132063d91c563928048fc83
Angelmir67894 <92046199+Angelmir67894@users.noreply.github.com>
2021-10-06 11:43:47 -0500

    Moon
    Added hello moon

--- GitHub Test/GitHub Test/Source.cpp
+++ GitHub Test/GitHub Test/Source.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {
 	string hello = "Hello World!";
+	string hello_world = "Hello World!";
+	string hello_moon = "Hello Moon!";
 
-	cout << hello;
+	cout << hello_world << endl;
+	cout << hello_moon << endl;
 }
